3e7e11f6a382578e8cfa0834dbdbb94bf4cf94ff
Sayanth <sayanth1188@gmail.com>
2023-12-11 07:44:51 -0800

    productview page section almost finished

--- Ecommerce/store/urls.py
+++ Ecommerce/store/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.Home,name="home"),
     path('brands/<str:slug>',views.Brands,name="brands"),
+    path('productview/<str:slug>',views.ProductView,name="productview"),
 ]
--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -14,6 +14,20 @@ def Brands(request,slug):
     category1=Category.objects.filter(slug=slug)
     subCategory=SubCategory.objects.filter(category__slug=slug)
     product=Product.objects.filter(category__slug=slug)
+    productSeries=ProductSeries.objects.filter(subcategory__slug=slug)
 
-    context={'category':category,'product':product,"subCategory":subCategory}  
+    all=[]
+    for p in product:
+        value =100-(p.Original_price/p.selling_price)*100
+        off_price = value
+        all.append(value)
+
+    context={'category':category,'product':product,"subCategory":subCategory,"productseries":productSeries,"all":all,}  
     return render(request,"store/brands.html",context)
+
+def ProductView(request,slug):
+    category=Category.objects.all()
+    product = Product.objects.filter(slug=slug)
+
+    context={'category':category,'product':product}  
+    return render(request,"store/productview.html",context)
